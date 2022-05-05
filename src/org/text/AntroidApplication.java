b8ce5519c6810a8e566a44e446d60a7e361c9f46
JEFFRINSINGH <jeffrinsingh05@gmail.com>
2022-05-05 12:08:58 +0530

    Third update

--- src/org/text/AntroidApplication.java
+++ src/org/text/AntroidApplication.java
@@ -4,6 +4,10 @@ public class AntroidApplication {
 	public static void main(String[] args) {
 		System.out.println("Java");
 		System.out.println("Python");
+		System.out.println("TestNG");
+		System.out.println("Cucumber");
+		System.out.println("SQL");
+		System.out.println("API");
 	}
 
 }
